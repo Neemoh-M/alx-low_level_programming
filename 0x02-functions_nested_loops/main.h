8e38d3f111b08405ea5bb49d16df79930d22b572
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-10-18 11:03:57 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,7 +4,7 @@
 
 void print_alphabet(void);
 void print_alphabet_x10(void);
-_putchar;
+int _putchar(char c);
 int _islower(int c);
 int _isalpha(int c);
 int print_sign(int n);
@@ -15,4 +15,4 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 
-endif
+#endif
