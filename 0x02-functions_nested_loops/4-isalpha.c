e74ca56dc773fe90af5d449c81d349233b5fc67c
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-10-18 13:53:15 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -9,7 +9,7 @@
  */
 int _isalpha(int c)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <=90))
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
 	{
 	return (1);
 	}
@@ -18,4 +18,4 @@ int _isalpha(int c)
 	return (0);
 	}
 	_putchar('\n');
-}	
+}
