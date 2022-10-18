0d586c5d550100cdc732056769044b6dade0ca25
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-10-18 11:15:25 -0700

    Functions_nested_loops

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,1 +1,21 @@
-Function nested loops
+#include "main.h"
+
+/**
+ * main- print 
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+	char p[] = "_putchar";
+
+	for (i = 0; i < 8; i++)
+		{
+		-putchar(p[i]);
+		}
+		-putchar('\n');
+	return (0);
+}
+
