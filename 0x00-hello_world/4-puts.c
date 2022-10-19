54ef11b60f8e4b471e76cbbb4aa2ae5ebc27a3e9
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-10-18 20:00:39 -0700

    Hello_world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,25 +1,11 @@
 #include <stdio.h>
-
-
-
 /*
+ * main - print a line of code using puts
  *
- *  * main - Entry point
- *
- *   *
- *
- *    * Return:0 (Success)
- *
- *     */
-
-
-
+ * Return:Always 0 (Success)    
+ */
 int main(void)
-
 {
-
 		puts("\"Programming is like building a multilingual puzzle\n");
-
 			return (0);
-
 }
