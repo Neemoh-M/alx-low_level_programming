4ee35e33756cb0d3f12eb570ea65d0a008963f54
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-10-19 03:14:58 -0700

    C - Variables, if, else, while

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Start of the code
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+
+	char c;
+
+	for (c = 'a'; c <= '2'; c++)
+		putchar(c);
+		putchar('\n');
+
+return (0);
+}
