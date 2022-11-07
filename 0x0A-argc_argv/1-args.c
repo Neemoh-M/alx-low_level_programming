f9efcba02964982ab94a9794ff16476a936cd04b
Neemoh-M <wairimumuigai.wm@gmail.com>
2022-11-07 15:33:47 +0300

    C - argc, argv

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
-
+/**
+ * main - Prints all the args content
+ * @argc: argument count
+ * @argv: argument vector
+ *
+ * Return: Always zero
+ */
 int main(int argc, char *argv[])
 {	int counter;
 
